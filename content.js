76c663f34ab08e48e30401c4189f4736646b2e62
Philip De Smedt <philip.desmedt@gmail.com>
2014-09-18 20:19:42 -0700

    Only execute onClick.

---
+++ content.js
@@ -0,0 +1,9 @@
+
+console.log("Company Name: " + $("#app_questions_name").val());
+console.log("Company URL: " + $("#app_questions_url").val());
+console.log($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
+console.log($('label[for="app_questions_make"]').text() + " " + $("#app_questions_make").val());
+console.log($('label[for="app_questions_rfs2"]').text() + " " + $("#app_questions_rfs2").val());
+
+console.log($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
+console.log($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
