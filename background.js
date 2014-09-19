7833012f261952b29c78820eaee189fa77bae7c6
Philip De Smedt <philip.desmedt@gmail.com>
2014-09-18 21:28:30 -0700

    Added basic error handling.

--- background.js
+++ background.js
@@ -8,6 +8,9 @@ chrome.browserAction.onClicked.addListener(function (activeTab) {
   chrome.tabs.executeScript(null, {
     file: "content.js"
   }, function (result) {
+    if (result == undefined) {
+      result = 'Please run this script in the tab of your YC application!';
+    }
     var htmlCode = "<html><body>" + result + "</body></html>";
     var url = "data:text/html," + encodeURIComponent(htmlCode);
     chrome.tabs.create({url: url});
