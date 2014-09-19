fb483bd25b287b972dc64d6174da0286000bf18d
Philip De Smedt <philip.desmedt@gmail.com>
2014-09-18 19:43:12 -0700

    first commit

---
+++ background.js
@@ -0,0 +1,11 @@
+//The code that is fired upon page load
+//to check your plugin js is working uncomment the next line.
+
+console.log("Company Name: " + $("#app_questions_name").val());
+console.log("Company URL: " + $("#app_questions_url").val());
+console.log($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
+console.log($('label[for="app_questions_make"]').text() + " " + $("#app_questions_make").val());
+console.log($('label[for="app_questions_rfs2"]').text() + " " + $("#app_questions_rfs2").val());
+
+console.log($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
+console.log($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
