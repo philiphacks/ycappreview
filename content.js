f18ba3a5356fc88b68f14bf4d72fdbeefb975f73
Philip De Smedt <philip.desmedt@gmail.com>
2014-09-18 21:20:28 -0700

    Done spitting out YC app to a new tab in HTML format.

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 var values = [];
 
-var labels = $("label")
+var labels = $("label");
 for (var i =0; i < labels.length; i++) { 
   var the_label = $(labels[i]); 
   var the_input = $("#" + $(labels[i]).attr("for"));
@@ -9,24 +9,15 @@ for (var i =0; i < labels.length; i++) {
   var label_txt = the_label.text();
   if (input_type !== undefined) {
     input_type = input_type.tagName;
-    //console.log(input_type);
+
     if (input_type == "SELECT") {
       var input_val = $("#" + $(labels[i]).attr("for") + " option:selected").text();
     } else {
       var input_val = the_input.val();
     }
 
-    console.log(label_txt + ": " + input_val);
+    values.push("<strong>" + label_txt + "</strong><p>" + input_val + "</p>");
   }
 }
-
-
-// values.push("Company Name: " + $("#app_questions_name").val());
-// values.push("Company URL: " + $("#app_questions_url").val());
-// values.push($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
-// values.push($('label[for="app_questions_make"]').text() + " " + $("#app_questions_make").val());
-// values.push($('label[for="app_questions_rfs2"]').text() + " " + $("#app_questions_rfs2").val());
-// values.push($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
-//values.push($('label[for="app_questions_demo"]').text() + " " + $("#app_questions_demo").val());
 
 values.join('<br/>');
