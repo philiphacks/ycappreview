4f3b40f5d913e56f908b6f43a9e7b1a4bcee51a0
Adel Qalieh <aqalieh95@gmail.com>
2014-10-01 18:34:42 -0400

    Change default font to sans-serif

--- js/background.js
+++ js/background.js
@@ -7,7 +7,7 @@ chrome.browserAction.onClicked.addListener(function (activeTab) {
     } else {
       result = '<div style="background-color:#FFF9AC"><h1>Instructions</h1><ul><li><a href="http://docs.google.com/document/create?hl=en" target="_blank">CREATE NEW GOOGLE DOCUMENT TO SHARE</a> (opens new browser tab) </li><li>Select all of the text below</li><li>Copy and Paste into Google Docs (or any other collaboration software)</li><li>Available on Github: <a href="https://github.com/philipdesmedt/ycappreview" target="_blank">https://github.com/philipdesmedt/ycappreview</a> (new tab)</li></ul><hr></div>' + result;
     }
-    var htmlCode = "<html><body>" + result + "</body></html>";
+    var htmlCode = "<html><body style='font-family: Helvetica, sans-serif'>" + result + "</body></html>";
     var url = "data:text/html," + encodeURIComponent(htmlCode);
     chrome.tabs.create({url: url});
   });
