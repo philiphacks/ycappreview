518973ffe1006e8eaa6451bbed5a2acc5fa386a4
Adel Qalieh <aqalieh95@gmail.com>
2014-10-01 18:53:52 -0400

    Change background color and margins a bit

--- js/background.js
+++ js/background.js
@@ -5,9 +5,9 @@ chrome.browserAction.onClicked.addListener(function (activeTab) {
     if (result == undefined) {
       result = 'ERROR: This script only works on <a href="https://apply.ycombinator.com/app/edit">https://apply.ycombinator.com/app/edit</a>!';
     } else {
-      result = '<div style="background-color:#FFF9AC"><h1>Instructions</h1><ul><li><a href="http://docs.google.com/document/create?hl=en" target="_blank">CREATE NEW GOOGLE DOCUMENT TO SHARE</a> (opens new browser tab) </li><li>Select all of the text below</li><li>Copy and Paste into Google Docs (or any other collaboration software)</li><li>Available on Github: <a href="https://github.com/philipdesmedt/ycappreview" target="_blank">https://github.com/philipdesmedt/ycappreview</a> (new tab)</li></ul><hr></div>' + result;
+      result = '<div style="margin-top: 0.3em;"><h1>Instructions</h1><ul><li><a href="http://docs.google.com/document/create?hl=en" target="_blank">CREATE NEW GOOGLE DOCUMENT TO SHARE</a> (opens new browser tab) </li><li>Select all of the text below</li><li>Copy and Paste into Google Docs (or any other collaboration software)</li><li>Available on Github: <a href="https://github.com/philipdesmedt/ycappreview" target="_blank">https://github.com/philipdesmedt/ycappreview</a> (new tab)</li></ul><hr></div>' + result;
     }
-    var htmlCode = "<html><head><link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'></head><body style='font-family: Raleway, Helvetica, sans-serif; max-width: 992px; margin: 0 auto;'>" + result + "</body></html>";
+    var htmlCode = "<html style='background-color: #f6f6ef;'><head><link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'></head><body style='font-family: Raleway, Helvetica, sans-serif; max-width: 992px; margin: 0 auto;'>" + result + "</body></html>";
     var url = "data:text/html," + encodeURIComponent(htmlCode);
     chrome.tabs.create({url: url});
   });
